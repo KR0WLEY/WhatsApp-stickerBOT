2381a6e8d478c7d5d4c7baf264c49052826837d1
KR0WLEY <50808176+KR0WLEY@users.noreply.github.com>
2020-07-18 18:25:08 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,53 @@
+const sulla = require('venom-bot');
+const mime = require('mime-types');
+const fs = require("fs");
+
+sulla.create().then((client) => start(client));
+ 
+function start(client) {
+
+  client.onMessage((message) => {
+
+    if(message.isMedia) {
+
+      const senderGRP = message.from;
+      if(message.isGroupMsg) {
+        
+        if(!message.caption.includes('/sticker')) return;
+
+        client.downloadFile(message).then(async bufer => {
+          const b = new Buffer.from(message.id.toString('base64').split('/').join('').slice(0, 17)+message.mediaKey.toString('base64').split('/').join('').slice(0, 15))
+
+          const filename = `figurinhas/${b.replace('false', '')}.${mime.extension(message.mimetype).includes("jpeg") ? mime.extension(message.mimetype).replace('jpeg', 'png') : mime.extension(message.mimetype)}`;
+
+          await fs.writeFile(filename, bufer, (err) => {
+  
+              if(err) return console.log(err)
+          })
+            setTimeout(() => { 
+              client.sendImageAsSticker(senderGRP, `C:/Users/samue/Desktop/bot-whatsapp/${filename}`) 
+
+          }, 750);
+
+        })
+        return;
+      }
+    
+      const sender = message.from;
+        
+        client.downloadFile(message).then(async buffer => {
+
+          const c = new Buffer.from(message.id.toString('base64').split('/').join('').slice(0, 15)+message.mediaKey.toString('base64').split('/').join('').slice(0, 15))
+          const fileName = `figurinhas/${c.replace('false', '')}.${mime.extension(message.mimetype)}`;
+  
+          await fs.writeFile(fileName, buffer, (err) => {
+              if(err) return console.log(err)
+              
+          })
+          setTimeout(() => { 
+              client.sendImageAsSticker(sender, `C:/Users/samue/Desktop/bot-whatsapp/${fileName}`) 
+          }, 750);
+        });
+      }
+    });
+  }
