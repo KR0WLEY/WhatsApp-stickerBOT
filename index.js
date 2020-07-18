5507bceb6ae5ca6d73c5e21e9ec9cb4febcaea8b
KR0WLEY <50808176+KR0WLEY@users.noreply.github.com>
2020-07-18 18:28:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function start(client) {
         client.downloadFile(message).then(async bufer => {
           const b = new Buffer.from(message.id.toString('base64').split('/').join('').slice(0, 17)+message.mediaKey.toString('base64').split('/').join('').slice(0, 15))
 
-          const filename = `figurinhas/${b.replace('false', '')}.${mime.extension(message.mimetype).includes("jpeg") ? mime.extension(message.mimetype).replace('jpeg', 'png') : mime.extension(message.mimetype)}`;
+          const filename = `figurinhas/${b.replace('false', Math.floor(Math.random() * 99998).toString())}.${mime.extension(message.mimetype).includes("jpeg") ? mime.extension(message.mimetype).replace('jpeg', 'png') : mime.extension(message.mimetype)}`;
 
           await fs.writeFile(filename, bufer, (err) => {
   
@@ -38,7 +38,7 @@ function start(client) {
         client.downloadFile(message).then(async buffer => {
 
           const c = new Buffer.from(message.id.toString('base64').split('/').join('').slice(0, 15)+message.mediaKey.toString('base64').split('/').join('').slice(0, 15))
-          const fileName = `figurinhas/${c.replace('false', '')}.${mime.extension(message.mimetype)}`;
+          const fileName = `figurinhas/${c.replace('false', Math.floor(Math.random() * 99999).toString())}.${mime.extension(message.mimetype)}`;
   
           await fs.writeFile(fileName, buffer, (err) => {
               if(err) return console.log(err)
