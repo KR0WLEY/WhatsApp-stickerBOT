1f379f0b384ec5deeba2c1f6b3907443c543db48
KR0WLEY <50808176+KR0WLEY@users.noreply.github.com>
2020-07-23 09:42:30 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,44 +9,47 @@ function start(client) {
   client.onMessage((message) => {
 
     if(message.isMedia) {
+
       if(message.isGroupMsg) {
         
         const senderGRP = message.from;
+
         if(!message.caption.includes('/sticker')) return;
 
-        client.downloadFile(message).then(async bufer => {
-          const b = new Buffer.from(message.id.toString('base64').split('/').join('').slice(0, 17)+message.mediaKey.toString('base64').split('/').join('').slice(0, 15))
+        client.decryptFile(message).then(async bufer => {
 
-          const filename = `figurinhas/${b.replace('false', Math.floor(Math.random() * 99998).toString())}.${mime.extension(message.mimetype).includes("jpeg") ? mime.extension(message.mimetype).replace('jpeg', 'png') : mime.extension(message.mimetype)}`;
+          const b = new Buffer.from(Math.floor(Math.random() * 99999).toString()+message.mediaKey.toString('base64').split('/').join('x').slice(0, 25))
+
+          const filename = `figurinhas/${b}.${mime.extension(message.mimetype)}`;
 
           await fs.writeFile(filename, bufer, (err) => {
   
               if(err) return console.log(err)
           })
             setTimeout(() => { 
-              client.sendImageAsSticker(senderGRP, `C:/Users/samue/Desktop/bot-whatsapp/${filename}`) 
+              client.sendImageAsSticker(senderGRP, `C:/Users/samue/Desktop/wpp-bot/${filename}`) 
 
           }, 750);
 
         })
         return;
-      }
-    
-      const sender = message.from;
-        
-        client.downloadFile(message).then(async buffer => {
+      } else {
 
-          const c = new Buffer.from(message.id.toString('base64').split('/').join('').slice(0, 15)+message.mediaKey.toString('base64').split('/').join('').slice(0, 15))
-          const fileName = `figurinhas/${c.replace('false', Math.floor(Math.random() * 99999).toString())}.${mime.extension(message.mimetype)}`;
+        const sender = message.from;
+        client.decryptFile(message).then(async buffer => {
+
+          const c = new Buffer.from(Math.floor(Math.random() * 99999).toString().slice(0, 18)+message.mediaKey.toString('base64').split('/').join('+-').slice(0, 15))
+          const fileName = `figurinhas/${c}.${mime.extension(message.mimetype)}`;
   
           await fs.writeFile(fileName, buffer, (err) => {
               if(err) return console.log(err)
               
           })
           setTimeout(() => { 
-              client.sendImageAsSticker(sender, `C:/Users/samue/Desktop/bot-whatsapp/${fileName}`) 
+              client.sendImageAsSticker(sender, `C:/Users/samue/Desktop/wpp-bot/${fileName}`) 
           }, 750);
         });
       }
-    });
-  }
+    }
+  });
+};
